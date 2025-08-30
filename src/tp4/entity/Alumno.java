b576be125377fa66e9c977862af8cb02fdfc3779
Mbarca89 <mauriciobarca1989@gmail.com>
2025-08-30 14:28:12 -0300

    Estructura basica

---
+++ src/tp4/entity/Alumno.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp4.entity;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+
+    public Alumno() {
+    }
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+    
+    
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void agregarMateria(Materia m) {
+        return;
+    }
+    
+    public int cantidadMaterias() {
+        return 0;
+    }
+    
+}
